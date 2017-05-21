58925237d257fd0676fda0e386b39332c849662d
Trevor Blades <tdblades@gmail.com>
2017-05-21 11:26:55 -0700

    Prefer object spread

--- react.js
+++ react.js
@@ -5,13 +5,17 @@ module.exports = {
     'prettier/react'
   ],
   parser: 'babel-eslint',
+  plugins: [
+    'prefer-object-spread'
+  ],
   rules: {
+    'jsx-a11y/no-static-element-interactions': 0,
+    'prefer-object-spread/prefer-object-spread': 2,
     'react/forbid-prop-types': 0,
     'react/jsx-filename-extension': 0,
     'react/jsx-sort-props': 2,
     'react/no-unescaped-entities': 0,
     'react/require-default-props': 0,
-    'react/sort-prop-types': 2,
-    'jsx-a11y/no-static-element-interactions': 0
+    'react/sort-prop-types': 2
   }
 };
