bc6fad79c499d08b8cd2dcc56a0a4fc403b470bb
Trevor Blades <tdblades@gmail.com>
2017-07-15 10:57:46 -0700

    Add a config for react-native

---
+++ react-native.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: './react.js',
+  plugins: [
+    'react-native'
+  ],
+  rules: {
+    'react-native/no-unused-styles': 2
+    'react-native/no-inline-styles': 2
+  }
+};
