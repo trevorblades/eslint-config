823217ef8b0be508cb7e005c9ca1ce4c74271042
Trevor Blades <tdblades@gmail.com>
2017-07-15 11:01:51 -0700

    Add a comma

--- react-native.js
+++ react-native.js
@@ -1,12 +1,10 @@
 module.exports = {
-  extends: [
-    './react.js'
-  ],
+  extends: './react.js',
   plugins: [
     'react-native'
   ],
   rules: {
-    'react-native/no-unused-styles': 2
+    'react-native/no-unused-styles': 2,
     'react-native/no-inline-styles': 2
   }
 };
