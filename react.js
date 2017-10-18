c9e106120b09ec07156d15089d82de7a190e7908
Trevor Blades <tdblades@gmail.com>
2017-10-17 17:56:15 -0700

    Don't use the click + key event a11y rule

--- react.js
+++ react.js
@@ -6,6 +6,7 @@ module.exports = {
   parser: 'babel-eslint',
   plugins: ['prefer-object-spread'],
   rules: {
+    'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
     'prefer-object-spread/prefer-object-spread': 'error',
     'react/forbid-prop-types': 'off',
