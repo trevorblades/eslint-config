18285b4efad62153a976cf9fa18c55743ce2e19f
Trevor Blades <tdblades@gmail.com>
2018-06-28 02:55:45 -0700

    Error on unused state

--- react.js
+++ react.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['./index.js', 'plugin:react/recommended'],
   rules: {
-    'react/no-unused-prop-types': 'error'
+    'react/no-unused-prop-types': 'error',
+    'react/no-unused-state': 'error'
   }
 };
