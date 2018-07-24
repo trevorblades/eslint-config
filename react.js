7a2db9762746fe7493e1ef50ec6447c80f40174e
Trevor Blades <tdblades@gmail.com>
2018-07-24 00:26:08 -0700

    Omit true boolean jsx props

--- react.js
+++ react.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['./index.js', 'plugin:react/recommended'],
   rules: {
     'react/no-unused-prop-types': 'error',
-    'react/no-unused-state': 'error'
+    'react/no-unused-state': 'error',
+    'react/jsx-boolean-value': 'error'
   }
 };
