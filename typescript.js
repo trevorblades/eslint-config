1e4da7a50d91c24ad2cdde0606acb9fbbe133d10
Trevor Blades <tdblades@gmail.com>
2021-09-26 18:00:00 -0700

    Add react hooks rules for typescript

--- typescript.js
+++ typescript.js
@@ -5,11 +5,14 @@ module.exports = {
     'plugin:@typescript-eslint/recommended'
   ],
   parser: '@typescript-eslint/parser',
+  plugins: ['react-hooks'],
   rules: {
     '@typescript-eslint/indent': 'off',
     '@typescript-eslint/explicit-module-boundary-types': [
       'warn',
       {allowTypedFunctionExpressions: false}
-    ]
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   }
 };
